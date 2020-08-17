4aea24584e4e7bcb6322c8ab466104132c821cf0
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2020-08-16 22:00:08 -0700

    refactored train

--- hw3/naive_bayes.py
+++ hw3/naive_bayes.py
@@ -6,8 +6,10 @@
 
 import sys
 import statistics as stat
+import numpy as np
 import math
 from recordclass import recordclass
+
 
 class NaiveBayesClassifier:
     """ Naive Bayes Classifier Object
@@ -41,93 +43,89 @@ class NaiveBayesClassifier:
             data = [line.rstrip('\n') for line in fp]
         return data
 
-    def find_stdDev(self, val: float, mean: float) -> float:
+    def std_dev(self, val: [], mean: float) -> float:
         """ Get the standard deviation """
-        sqrDiff = []
+        diff = []
 
         for i in val:
-            sqrDiff.append((i - mean) ** 2)
+            diff.append((i - mean) ** 2)
 
-        variance = ((1 / (len(sqrDiff) - 1)) * sum(sqrDiff))
-        if (variance < 0.0001):
-            variance = 0.0001
+        var = ((1 / (len(diff) - 1)) * sum(diff))
+        if (var < 0.0001):
+            var = 0.0001
 
-        return math.sqrt(variance)
+        return math.sqrt(var)
 
-    def calc_gaussian(self, x: float, mean: float, stdev: float) -> float:
+    def gaussian(self, x: float, mean: float, stdev: float) -> float:
         """ Calculate Gaussian """
         a = 1 / (stdev * math.sqrt(2 * math.pi))
         b = math.exp(-1 * (((x - mean) ** 2) / (2 * (stdev ** 2))))
-        return a * b
 
-    def getAccuracy(self, classProbs: [], trueClass: int) -> float:
+        return a * b
+        #return np.exp(-np.power((x - mean) / stdev, 2.) / 2.)
+
+    def accuracy(self, class_probs: [], actual_class: int) -> float:
         """ Get the accuracy
         Given the class probabilities and the true class labels,
         calculate the accuracy of our classifier.
         """
-        maxes = []
+        maximums = []
 
-        for i, prob in enumerate(classProbs):
-            if (prob == max(classProbs)):
-                maxes.append(i + 1)
+        for i, prob in enumerate(class_probs):
+            if (prob == max(class_probs)):
+                maximums.append(i + 1)
 
-        if (len(maxes) == 1):
-            if (maxes[0] == trueClass):
+        if (len(maximums) == 1):
+            if (maximums[0] == actual_class):
                 return 1
             else:
                 return 0
         else:
-            if (trueClass in maxes):
-                return 1 / len(maxes)
+            if (actual_class in maximums):
+                return 1 / len(maximums)
             else:
                 return 0
 
-    def train(self, data=[]) -> None:
+    def train(self, data: []) -> None:
         """ Train a Gaussian Naive Bayes Classifier
         """
-        classNums = []
+        class_ids = []
 
-        # Determine # of classes
+        # Get the number of classes
         for i in data:
-            tempStr = i.split()
-            temp = [float(x) for x in tempStr]
+            fields = i.split()
+            fields = [float(x) for x in fields]
 
-            if (not (temp[-1] in classNums)):
-                classNums.append(temp[-1])
+            if (not (fields[-1] in class_ids)):
+                class_ids.append(fields[-1])
 
-        # Create class objects and associated attribute objects
-        for i in range(0, len(classNums)):
+        # Create classifiers
+        for i in range(0, len(class_ids)):
             c = self.Classifier(classID=i+1, prob=float(), attributes=[])
             self.classes.append(c)
 
-            # Get number of columns in the data
             for j in range(0, len(data[0].split()) - 1):
                 a = self.Attribute(attributeID=j+1, mean=float(), stdDev=float(), prob=float, values=[])
                 self.classes[i].attributes.append(a)
 
-        # Find and file values to associated attribute object
+        # Calculate probailities
         for i in data:
-            tempStr = i.split()
-            temp = [float(x) for x in tempStr]
-            clNum = int(temp[-1])
+            fields = i.split()
+            fields = [float(x) for x in fields]
+            class_id = int(fields[-1])
 
             for j in self.classes:
-                if (j.classID == clNum):
-                    for index, k in enumerate(temp[:-1]):
+                if (j.classID == class_id):
+                    for index, k in enumerate(fields[:-1]):
                         j.attributes[index].values.append(k)
                 j.prob = len(j.attributes[0].values) / len(data)
 
-                # for k in j.attributes:
-                #     if (not (len(k.values) == 0)):
-                #         k.mean = stat.mean(k.values)
-                #     k.stdDev = self.find_stdDev(k.values, k.mean)
-
-        # Calulate mean and standard deviation for each attribute
+        # Get the mean and std dev
         for i in self.classes:
             for j in i.attributes:
                 if (not (len(j.values) == 0)):
                     j.mean = stat.mean(j.values)
-                j.stdDev = self.find_stdDev(j.values, j.mean)
+                j.stdDev = self.std_dev(j.values, j.mean)
 
     def classify(self, data: []) -> float:
         """ Classify data based on trained Naive Bayes"""
@@ -143,7 +141,7 @@ class NaiveBayesClassifier:
 
                 for ind, k in enumerate(temp[:-1]):
                     tempAtt = self.classes[j].attributes[ind]
-                    p_xGivenC *= self.calc_gaussian(k, tempAtt.mean, tempAtt.stdDev)
+                    p_xGivenC *= self.gaussian(k, tempAtt.mean, tempAtt.stdDev)
 
                 sample.p_xGivenCs.append(p_xGivenC)
                 sample.p_x += (sample.p_xGivenCs[j] * self.classes[j].prob)
@@ -157,7 +155,7 @@ class NaiveBayesClassifier:
             # Take the max value as the identified class
             sample.prob = max(classProbs)
             sample.pClass = classProbs.index(max(classProbs)) + 1
-            sample.accuracy = self.getAccuracy(classProbs, sample.tClass)
+            sample.accuracy = self.accuracy(classProbs, sample.tClass)
             print("ID=%5d, predicted=%3d, probability = %.4f, true=%3d, accuracy=%4.2f" % (
             sample.id, sample.pClass, sample.prob, sample.tClass, sample.accuracy))
 
